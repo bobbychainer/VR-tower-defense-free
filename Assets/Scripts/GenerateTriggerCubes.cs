9099622c03c75686eda5b912ad3ff0a2c812fe5b
bobbychainer <bobbychainer@gmail.com>
2024-05-08 17:14:18 +0200

    isTrigger-Cubes height fixed

--- Assets/Scripts/GenerateTriggerCubes.cs
+++ Assets/Scripts/GenerateTriggerCubes.cs
@@ -3,35 +3,34 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class TriggerColliderGenerator : MonoBehaviour {
-    public GameObject triggerColliderPrefab; // Das Prefab für den Trigger-Collider-Block
-    public Vector2 gridSize; // Die Größe des Gitters (z. B. 50x50)
-    public Material cubeMaterial; // Das Material für die Würfel
+    public GameObject triggerColliderPrefab; // prefab
+    public Vector2 gridSize; // grid size
+    public Material cubeMaterial; // cube material
 
-    public float size = 0.1f;
+    public float height = 0.1f;
 
     void Start() {
         GenerateTriggerColliders();
     }
 
     void GenerateTriggerColliders() {
-        // Iteriere über jede Position auf der Ebene darüber
         for (float x = 0.5f; x < gridSize.x; x++) {
-            // Erzeuge ein leeres GameObject als Elternobjekt für die aktuelle Reihe
+            // Create row parent
             GameObject rowParent = new GameObject("Row " + (x - 0.5f));
-            rowParent.transform.parent = transform; // Setze das Elternobjekt in die Hierarchie
+            rowParent.transform.parent = transform; 
 
             for (float z = 0.5f; z < gridSize.y; z++) {
-                GameObject cube = Instantiate(triggerColliderPrefab, new Vector3(x, 1.5f, z), Quaternion.identity);
+                GameObject cube = Instantiate(triggerColliderPrefab, new Vector3(x, 1f + (height / 2), z), Quaternion.identity);
                 cube.name = "Cube " + x.ToString() + " | " + z.ToString();
                 cube.AddComponent<BoxCollider>().isTrigger = true;
                 cube.transform.parent = rowParent.transform;
                 
-                Renderer cubeRenderer = cube.GetComponent<Renderer>(); // Füge das Material zum Cube hinzu
+                Renderer cubeRenderer = cube.GetComponent<Renderer>(); // Add material
                 if (cubeRenderer != null && cubeMaterial != null) {
                     cubeRenderer.material = cubeMaterial;
                 }
             }
-            DontDestroyOnLoad(rowParent);
+            // DontDestroyOnLoad(rowParent); TODO: fix me maybe
         }
         Debug.Log("Fertig");
     }
