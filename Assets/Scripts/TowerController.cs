d14ef8c97d33c817b30eeb0324d0a5f8391a87a0
bobbychainer <bobbychainer@gmail.com>
2024-05-11 16:19:10 +0200

    Tower added

---
+++ Assets/Scripts/TowerController.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerController : MonoBehaviour {
+	
+	private int attackSpeed = 5;
+	private int health = 20;
+	
+	
+	
+}
