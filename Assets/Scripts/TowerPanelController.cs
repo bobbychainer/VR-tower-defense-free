9980893cdc60dd2385c13137493887fe528ba52b
bobbychainer <bobbychainer@gmail.com>
2024-07-04 16:20:27 +0200

    Tower Panel skaliert auf Distanz zwischen Objekt und Kamera

--- Assets/Scripts/TowerPanelController.cs
+++ Assets/Scripts/TowerPanelController.cs
@@ -4,18 +4,50 @@ using UnityEngine;
 
 public class TowerPanelController : MonoBehaviour {
 	
-	private Camera cam;
+	private Camera mainCamera; // The other GameObject to measure the distance to
+    private float minScale = 0.8f; // Minimum scale value
+    private float maxScale = 2.0f; // Maximum scale value
+	private float distanceScale = 0.1f;
+	
+	private Vector3 initialScale;
+	
     // Start is called before the first frame update
-    void Start() {
-        cam = Camera.main;
+    void Start() {		
+		mainCamera = Camera.main;
+		
+		if (mainCamera == null) {
+			Debug.LogError("Target object not set.");
+			return;
+		}
+        initialScale = transform.localScale;
     }
 
     // Update is called once per frame
     void Update() {
         // face camera
-		Vector3 directionToCamera = transform.position - cam.transform.position;
-        Vector3 lookPosition = transform.position + directionToCamera;
-
-        transform.LookAt(lookPosition);
+		LookToCamera();
+		ScaleToCamera();
     }
+	
+	private void LookToCamera() {
+		if (mainCamera != null) {			
+			Vector3 directionToCamera = transform.position - mainCamera.transform.position;
+			Vector3 lookPosition = transform.position + directionToCamera;
+
+			transform.LookAt(lookPosition);
+		}
+	}
+	
+	private void ScaleToCamera() {
+		if (mainCamera != null) {
+			float distance = Vector3.Distance(transform.position, mainCamera.transform.position);
+			
+			float scaleFactor = distance * distanceScale;
+			
+			if (scaleFactor < minScale) scaleFactor = minScale;
+			if (scaleFactor > maxScale) scaleFactor = maxScale;			
+			
+			transform.localScale = initialScale * scaleFactor;
+		}
+	}
 }
